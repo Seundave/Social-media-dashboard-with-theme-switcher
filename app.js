d6cf4c658c408c2bdef22eff5765a73f193a7911
David <olayinkadavid62@yahoo.com>
2022-06-23 18:08:45 +0100

    Styled the light mode of the theme switcher

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const toggle = document.querySelector(".toggle");
 const toggleBtn = document.querySelector(".toggle_btn");
 const Body = document.querySelector("body");
-const Header = document.querySelector("header")
 
 toggle.addEventListener("click", (e) => {
     toggleBtn.classList.toggle("night-mode")
